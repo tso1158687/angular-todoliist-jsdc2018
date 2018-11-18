df54781e689ec108a2b1dfa7803a10c8d6a7924b
jason <jason@wishingsoft.com>
2018-11-18 11:26:24 +0800

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  newTodo: string;
+  title = '待辦事項';
+  placeholder = '還有什麼事情沒有做';
+  showTodos = true;
+  todos: any[];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getData()
+    .subscribe(e => this.todos = e);
+  }
+  addNewTodo() {
+    console.log(this.newTodo);
+    this.todos.push({
+      name: this.newTodo,
+      status: false
+    });
+    this.newTodo = '';
   }
 
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
 }
